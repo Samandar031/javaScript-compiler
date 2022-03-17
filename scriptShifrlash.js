1d461ca9bafc27bd4c35f95f7626612955afab31
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-03-17 14:26:27 +0500

    boshladik

--- scriptShifrlash.js
+++ scriptShifrlash.js
@@ -2,7 +2,11 @@
 
 let btnShifrlash = document.querySelector(".btn");
 let btnDeshifrlash = document.querySelector(".btn-1");
-let btnLotin = document.querySelector(".btn-2");
+let btnLotin = document.querySelector(".btn-lotin");
+
+let btnClear = document.querySelector(".btn-clear");
+let btnAnim = document.querySelector(".btn-anim");
+let btnKril = document.querySelector(".btn-kril");
 
 let inputOne = document.querySelector(".input_one");
 let inputTwo = document.querySelector(".input_two");
@@ -79,6 +83,64 @@ let lugat = new Map([
   ["o',", "у"],
   ["sh", "ш"],
   ["ch", "ч"],
+
+  //
+  ["A", "а"],
+  ["B", "б"],
+  ["D", "д"],
+  ["E", "э"],
+  ["F", "ф"],
+  ["G", "г"],
+  ["H", "х"],
+  ["I", "и"],
+  ["J", "ж"],
+  ["K", "к"],
+  ["L", "л"],
+  ["M", "м"],
+  ["N", "Н"],
+  ["O", "о"],
+  ["P", "п"],
+  ["Q", "к"],
+  ["R", "р"],
+  ["S", "с"],
+  ["T", "т"],
+  ["U", "у"],
+  ["V", "в"],
+  ["X", "х"],
+  ["Y", "е"],
+  ["Z", "з"],
+  ["O',", "у"],
+  ["SH", "ш"],
+  ["CH", "ч"],
+  //
+
+  ["а", "a"],
+  ["б", "b"],
+  ["д", "d"],
+  ["э", "e"],
+  ["ф", "f"],
+  ["г", "g"],
+  ["х", "h"],
+  ["и", "i"],
+  ["ж", "j"],
+  ["к", "k"],
+  ["л", "l"],
+  ["м", "m"],
+  ["Н", "n"],
+  ["о", "o"],
+  ["п", "p"],
+  ["к", "q"],
+  ["р", "r"],
+  ["с", "s"],
+  ["т", "t"],
+  ["у", "u"],
+  ["в", "v"],
+  ["х", "x"],
+  ["е", "y"],
+  ["з", "z"],
+  ["у", "o'"],
+  ["ш", "sh"],
+  ["ч", "ch"],
 ]);
 
 btnLotin.addEventListener("click", function (c) {
@@ -98,4 +160,32 @@ btnLotin.addEventListener("click", function (c) {
   let kril = arr.join("");
   console.log(kril);
   inputThree.value = kril;
+  inputOne.value = kril;
+});
+
+// let inter = setInterval(function (l) {
+//   inputOne.value = inputThree.value;
+// }, 10000);
+
+btnKril.addEventListener("click", function (e) {
+  let Kirilcha = inputThree.value;
+  let KirilLotin = Kirilcha.split("");
+
+  let kir = [];
+  for (let e = 0; e < KirilLotin.length; e++) {
+    if (lugat.has(KirilLotin[e])) {
+      kir.push(lugat.get(KirilLotin[e]));
+    } else {
+      kir.push(KirilLotin[e]);
+    }
+  }
+  let lotin = kir.join("");
+  console.log(lotin);
+  inputOne.value = lotin;
+});
+
+btnClear.addEventListener("click", function (k) {
+  inputOne.value = "";
+  inputTwo.value = "";
+  inputThree.value = "";
 });
